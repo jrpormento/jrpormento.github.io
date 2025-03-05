4d93d4ea4322c00b2c0a4ad2cc74a517a2963d46
Jethrel Pormento <75268427+jrpormento@users.noreply.github.com>
2025-03-05 20:04:11 -0330

    Added a description for search engine results and cleaned up comments

--- static/js/resumeButton.js
+++ static/js/resumeButton.js
@@ -1,19 +1,18 @@
-// Get elements
 const openResumeBtn = document.getElementById('openResumeBtn');
 const resumeModal = document.getElementById('resumeModal');
 const closeBtn = document.getElementById('closeBtn');
 
-// Open the modal when the button is clicked
+// Opens modal
 openResumeBtn.addEventListener('click', function() {
   resumeModal.style.display = 'block';
 });
 
-// Close the modal when the close button (X) is clicked
+// Closes modal when exit clicked
 closeBtn.addEventListener('click', function() {
   resumeModal.style.display = 'none';
 });
 
-// Close the modal if the user clicks outside the modal content
+// Closes modal if content outside modal is clicked
 window.addEventListener('click', function(event) {
   if (event.target === resumeModal) {
     resumeModal.style.display = 'none';
